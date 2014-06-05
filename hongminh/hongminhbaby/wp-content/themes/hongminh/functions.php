0cb2c488986c90c772c4303859fb8825e3830610
Dung <tiendungdcn@gmail.com>
2014-06-05 11:00:02 +0700

    Dung

--- hongminh/hongminhbaby/wp-content/themes/hongminh/functions.php
+++ hongminh/hongminhbaby/wp-content/themes/hongminh/functions.php
@@ -94,9 +94,43 @@ add_action( 'widgets_init', 'hongminh_widgets_init' );
 function hongminh_scripts() {
 	wp_enqueue_style( 'hongminh-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'hongminh-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+    //Add css hongminh
+    wp_enqueue_style( 'basic', get_template_directory_uri() . '/css/basic.css', array(), '1.0.0' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '1.0.0' );
+    wp_enqueue_style( 'color-1', get_template_directory_uri() . '/css/color-1.css', array(), '1.0.0' );
+    wp_enqueue_style( 'cp_load', get_template_directory_uri() . '/css/cp_load.css', array(), '1.0.0' );
+    wp_enqueue_style( 'custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0' );
+    wp_enqueue_style( 'easy-responsive', get_template_directory_uri() . '/css/easy-responsive.css', array(), '1.0.0' );
+    wp_enqueue_style( 'font-awesome.min', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '1.0.0' );
+    wp_enqueue_style( 'jquery.bxslider', get_template_directory_uri() . '/css/jquery.bxslider.css', array(), '1.0.0' );
+    wp_enqueue_style( 'jquery.mCustomScrollbar', get_template_directory_uri() . '/css/jquery.mCustomScrollbar.css', array(), '1.0.0' );
+    wp_enqueue_style( 'light-box', get_template_directory_uri() . '/css/light-box.css', array(), '1.0.0' );
+    wp_enqueue_style( 'prettyPhoto', get_template_directory_uri() . '/css/prettyPhoto.css', array(), '1.0.0' );
+    wp_enqueue_style( 'slider-range', get_template_directory_uri() . '/css/slider-range.css', array(), '1.0.0' );
 
+
+    // Add js hongminh
+	wp_enqueue_script( 'hongminh-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 	wp_enqueue_script( 'hongminh-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'basic', get_template_directory_uri() . '/js/basic.js', array(), '20140606', true );
+	wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20140606', true );
+	wp_enqueue_script( 'cp_load', get_template_directory_uri() . '/js/cp_load.js', array(), '20140606', true );
+	wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array(), '20140606', true );
+	wp_enqueue_script( 'cycle-slider', get_template_directory_uri() . '/js/cycle-slider.js', array(), '20140606', true );
+	wp_enqueue_script( 'easy-slider.min', get_template_directory_uri() . '/js/easy-slider.min.js', array(), '20140606', true );
+	wp_enqueue_script( 'jquery.bxslider.min', get_template_directory_uri() . '/js/jquery.bxslider.min.js', array(), '20140606', true );
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', array(), '20140606', true );
+	wp_enqueue_script( 'jquery.mCustomScrollbar', get_template_directory_uri() . '/js/jquery.mCustomScrollbar.js', array(), '20140606', true );
+	wp_enqueue_script( 'jquery.simplemodal', get_template_directory_uri() . '/js/jquery.simplemodal.js', array(), '20140606', true );
+	wp_enqueue_script( 'mas-jquery', get_template_directory_uri() . '/js/mas-jquery.js', array(), '20140606', true );
+	wp_enqueue_script( 'mouse', get_template_directory_uri() . '/js/mouse.js', array(), '20140606', true );
+	wp_enqueue_script( 'prettyph', get_template_directory_uri() . '/js/prettyph.js', array(), '20140606', true );
+	wp_enqueue_script( 'respond', get_template_directory_uri() . '/js/respond.js', array(), '20140606', true );
+	wp_enqueue_script( 'slider', get_template_directory_uri() . '/js/slider.js', array(), '20140606', true );
+	wp_enqueue_script( 'underscore-min', get_template_directory_uri() . '/js/underscore-min.js', array(), '20140606', true );
+	wp_enqueue_script( 'widget', get_template_directory_uri() . '/js/widget.js', array(), '20140606', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
